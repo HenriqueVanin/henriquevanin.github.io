13660a104e1f0d3f3a0f4245c1cff9d5cc7694d8
HenriqVanin <henrique@driva.com.br>
2023-04-23 19:59:47 -0300

    Update: Script sh read

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import Oficinas2 from "./projects/Oficinas2";
 
+import { exec } from "child_process";
+export var yourscript = exec("sh hi.sh", (error, stdout, stderr) => {
+  console.log(stdout);
+  console.log(stderr);
+  if (error !== null) {
+    console.log(`exec error: ${error}`);
+  }
+});
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Oficinas2 />
+        {yourscript}
       </header>
     </div>
   );
